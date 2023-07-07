257662f685b73be5c01780b55da5f4d1c862c967
Den221B <drakhov@gmail.com>
2023-07-07 09:45:45 +0300

    Task1

---
+++ Tasks/src/task1.rs
@@ -0,0 +1,46 @@
+/*
+----> ЗАДАНИЕ 1 "Поиск слова в строке"
+
+Вывести номер строки в котором встречается нужное слово и саму строку в формате:
+номер строки: строка...
+
+ */
+
+use std::ptr::null;
+
+const SEARCH_TERM: &str = "picture";
+const QUOTE: &str = "Every face, every shop, bedroom window, public-house, and
+dark square is a picture feverishly turned--in search of what?
+It is the same with books. What do we seek through millions of pages?";
+
+
+fn main() {
+    find_term(SEARCH_TERM, QUOTE);
+}
+
+fn find_term(search_term: &str, quote: &str) -> String {
+    let mut n_line= 1;
+    for line in quote.lines() {
+        if line.contains(search_term) {
+            //println!("{}: {}", n_line, line);
+            return format!("{}: {}", n_line, line);
+        };
+        n_line+=1
+    }
+    "".to_string()
+}
+
+
+// ----> TESTS
+#[cfg(test)]
+mod tests {
+    use crate::find_term;
+    use crate::{SEARCH_TERM, QUOTE};
+
+    #[test]
+    fn correct_line() {
+        let answer = find_term(SEARCH_TERM, QUOTE);
+
+        assert_eq!("2: dark square is a picture feverishly turned--in search of what?", answer)
+    }
+}
